4742a33a39dc106dae3ef98c369a47a81926274a
Pavel Bobkov <microsmaster@rambler.ru>
2018-05-26 10:02:56 +0300

    initial commit

---
+++ app/src/main/java/ru/chipenable/openglhockey/FirstOpenGLProjectActivity.kt
@@ -0,0 +1,13 @@
+package ru.chipenable.openglhockey
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstOpenGLProjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_opengl_project)
+    }
+
+}
